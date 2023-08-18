59d6dbedf26c0dc2fc8a238a7d7b34f4d96f7c25
anickacodes <anickalewis@pursuit.org>
2023-08-18 03:15:43 -0400

    Initial Commit, Added API key to env to gi, installed vite

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+      
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+     
+      </div>
+    
+      {/* fetch(
+  `https://youtube.googleapis.com/youtube/v3/search?key=${process.env.YOUTUBE_APP_API_KEY}`
+); */}
+    </>
+  )
+}
+
+export default App

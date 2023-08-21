1dfeb2370c69c5232801b610ce7aa15431086ae8
anickacodes <anickalewis@pursuit.org>
2023-08-21 10:51:56 -0400

    reworked nav component, styling

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,19 +4,38 @@ import logo from "../assets/ytlogoglittered.jpeg";
 
 const NavBar = () => {
   return (
-    <div className="container" >
-    <nav >
-      <Link to={"/"} className="site-title"> <img src={logo} id="logo" width={70}/> YouTube </Link>
-      <ul>
-     
-        <li className="active">
-            <Link to={'/home'}>Home</Link>
-        </li>
-        <li className="active">
-            <Link to={'/About'}>About</Link>
-        </li>
-      </ul>
-    </nav>
+    <div className="container">
+      <nav>
+        <div id="logo-link">
+          <Link to={"/"} className="site-title">
+            {" "}
+            <img src={logo} id="logo" width={70} /> YouTube{" "}
+          </Link>
+        </div>
+
+        <div className="nav-links">
+          {" "}
+          <ul>
+            <li className="active">
+              <Link to={"/home"}>Home</Link>
+              {/* should render fresh viideos */}
+            </li>
+            <li className="active">
+              <Link to={"/About"}>About</Link>
+              {/* should  */}
+            </li>
+          </ul>
+        </div>
+        <div className="">
+          <button className="button">
+            <div className="button-container">
+                <img al/>
+              {/* <div className="top_left">top-left</div> */}
+              {/* <div className="top_right">top-right</div> */}
+            </div>
+          </button>
+        </div>
+      </nav>
     </div>
   );
 };

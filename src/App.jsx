bf241ad5ec8ea672d697cb3a7eacc590496bd031
anickacodes <anickalewis@pursuit.org>
2023-08-22 11:34:25 -0400

    imported route, routes,, updated apoutpage route

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,34 @@
-
 import { useState, useEffect } from "react";
 import "./App.css";
 import NavBar from "./components/NavBar";
+import { Route, Routes } from "react-router-dom";
+import AboutPage from "./components/AboutPage";
 // import YouTubeAPIComponent from "./components/YouTubeAPIComponent";
-
 
 function App() {
   return (
-
-
     <>
-      {/* add to all pages */}
       <header>
         <NavBar />
       </header>
 
-      <div> <h1>IMBAD </h1></div>
+      <div>
+        {" "}
+        <h1>IMBAD </h1>
+      </div>
+      <Routes>
+        <Route path="/home" element={<h1>Home - Welcome to IMBAD </h1>} />
+        <Route path="/AboutPage" element={<AboutPage />} />
+      </Routes>
 
-     
       <div className="card videos">
         <div>
           <h4>videos section</h4>
           {/* <YouTubeAPIComponent /> */}
         </div>
       </div>
-
-  
-  )
-
     </>
   );
-
 }
 
 export default App;
-
-

185acdb85dff97216076627cb167a5b7104f0db2
Fig <bethanytrr@gmail.com>
2023-08-22 11:37:23 -0400

    Added some changes for legibility and updated logo.

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,29 @@
-
-import { useState, useEffect } from "react";
-import "./App.css";
-import NavBar from "./components/NavBar";
+import { useState, useEffect } from 'react';
+import './App.css';
+import NavBar from './components/NavBar';
 // import YouTubeAPIComponent from "./components/YouTubeAPIComponent";
 
-
 function App() {
-  return (
+	return (
+		<>
+			{/* add to all pages */}
+			<header>
+				<NavBar />
+			</header>
 
+			<div>
+				{' '}
+				<h1>IMBAD</h1>
+			</div>
 
-    <>
-      {/* add to all pages */}
-      <header>
-        <NavBar />
-      </header>
-
-      <div> <h1>IMBAD </h1></div>
-
-     
-      <div className="card videos">
-        <div>
-          <h4>videos section</h4>
-          {/* <YouTubeAPIComponent /> */}
-        </div>
-      </div>
-
-  
-  )
-
-    </>
-  );
-
+			<div className="card videos">
+				<div>
+					<h4>Videos Section</h4>
+					{/* <YouTubeAPIComponent /> */}
+				</div>
+			</div>
+		</>
+	);
 }
 
 export default App;
-
-
--- src/components/YouTubeAPIComponent.jsx
+++ src/components/YouTubeAPIComponent.jsx
@@ -1,45 +1,50 @@
 import React, { useState, useEffect } from 'react';
 
 function YouTubeAPIComponent() {
-    const [videos, setVideos] = useState([]);
-  
-    useEffect(() => {
-      const API_KEY = 
-      `${import.meta.env.VITE_REACT_APP_API_KEY}`;
-      // console.log('API_KEY:', API_KEY);
-    
-      const API_URL = `https://www.googleapis.com/youtube/v3/search?key=${API_KEY}&part=snippet&q=beyonce`;
-      // console.log('API_URL:', API_URL);
-  
-      fetch(API_URL)
-        .then(response => {
-          if (!response.ok) {
-            throw new Error(`HTTP error! Status: ${response.status} - ${response.statusText}`);
-          }
-          return response.json();
-        })
-        .then(data => {
-          setVideos(data.items);
-        })
-        .catch(error => {
-          console.error('Error fetching data from YouTube API:', error);
-        });
-    }, []);
-  
-    return (
-      <div>
-        <h1>YouTube Videos</h1>
-        <ul>
-          {videos.map(video => (
-            <li key={video.id.videoId}>
-              <a href={`https://www.youtube.com/watch?v=${video.id.videoId}`} target="_blank" rel="noopener noreferrer">
-                {video.snippet.title}
-              </a>
-            </li>
-          ))}
-        </ul>
-      </div>
-    );
-  }
-  
-  export default YouTubeAPIComponent;
+	const [videos, setVideos] = useState([]);
+
+	useEffect(() => {
+		const API_KEY = `${import.meta.env.VITE_REACT_APP_API_KEY}`;
+		// console.log('API_KEY:', API_KEY);
+
+		const API_URL = `https://www.googleapis.com/youtube/v3/search?key=${API_KEY}&part=snippet&q=beyonce`;
+		// console.log('API_URL:', API_URL);
+
+		fetch(API_URL)
+			.then((response) => {
+				if (!response.ok) {
+					throw new Error(
+						`HTTP error! Status: ${response.status} - ${response.statusText}`
+					);
+				}
+				return response.json();
+			})
+			.then((data) => {
+				setVideos(data.items);
+			})
+			.catch((error) => {
+				console.error('Error fetching data from YouTube API:', error);
+			});
+	}, []);
+
+	return (
+		<div>
+			<h1>YouTube Videos</h1>
+			<ul>
+				{videos.map((video) => (
+					<li key={video.id.videoId}>
+						<a
+							href={`https://www.youtube.com/watch?v=${video.id.videoId}`}
+							target="_blank"
+							rel="noopener noreferrer"
+						>
+							{video.snippet.title}
+						</a>
+					</li>
+				))}
+			</ul>
+		</div>
+	);
+}
+
+export default YouTubeAPIComponent;

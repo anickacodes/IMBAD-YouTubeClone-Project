29e1529bcfacb8ca813135ca056b8daf87a1da59
anickacodes <anickalewis@pursuit.org>
2023-08-22 11:57:54 -0400

    imported route, routes; updated path to aboutPage

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 import { useState, useEffect } from 'react';
 import './App.css';
 import NavBar from './components/NavBar';
+import AboutPage from './components/AboutPage';
 // import YouTubeAPIComponent from "./components/YouTubeAPIComponent";
+import { Route, Routes } from 'react-router-dom';
 
 function App() {
 	return (
@@ -15,7 +17,11 @@ function App() {
 				{' '}
 				<h1>IMBAD</h1>
 			</div>
-
+      <Routes>
+        <Route path="/home" element={<h1>Home - Welcome to IMBAD </h1>} />
+        <Route path="/AboutPage" element={ <AboutPage />} />
+      
+      </Routes>
 			<div className="card videos">
 				<div>
 					<h4>Videos Section</h4>

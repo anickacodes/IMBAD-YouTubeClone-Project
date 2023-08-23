d3fcfd0fa6f005371e92df6102f12dc0bd73ac05
marciafoster <mfoster9tre@gmail.com>
2023-08-22 23:21:36 -0400

    updated about page, added images

--- src/components/AboutPage.jsx
+++ src/components/AboutPage.jsx
@@ -1,52 +1,71 @@
-import React from 'react';
+import React from "react";
 
 const AboutPage = () => {
-    return(
-        <div>
-        <h1>About IMBAD</h1>
-        <p>
-            IMBAD "I'M BAD" is a team of five talented and determined women who are Pursuit fellows, training to become full-stack software developers. 
-            Together we are utilizing the React skills we have learned thus far to create a "Youtube Clone" application. Using YouTube's API, we're crafting a platform that allows users to seamlessly explore, search, and play YouTube videos directly on our application. 
-            As we navigate the intricacies of integrating APIs and refining user interfaces, we're building a space that mirrors the functionality of the renowned YouTube platform.
-        </p>
-        
-        <h2>IRIS</h2>
-        <p>
+  return (
+    <div>
+      <h1>About IMBAD</h1>
+      <p>
+        IMBAD (I'M BAD) is a team of five talented and determined women who are
+        Pursuit fellows, training to become full-stack software developers.
+        Together we are utilizing the React skills we have learned thus far to
+        create a "Youtube Clone" application. Using YouTube's API, we're
+        crafting a platform that allows users to seamlessly explore, search, and
+        play YouTube videos directly on our application. As we navigate the
+        intricacies of integrating APIs and refining user interfaces, we're
+        building a space that mirrors the functionality of the renowned YouTube
+        platform.
+      </p>
 
-        </p>
-			<h2>MARCIA</h2>
-			<p>
-				Marcia is a driven home -based bakery owner who's passionate about
-				creating custom cakes and treats. As a Pursuit fellow on the path to
-				full-stack software development, she merges her love for crafting from
-				scratch with her desire of learning to code. Marcia enjoys traveling,
-				exploring new cultures, and trying new foods.
-			</p>
-			<h2>BETHANY</h2>
-			<p>
-				Bethany (nicknamed Fig) is a freelance illustrator and multimedia artist
-				who has interest in all forms of expression and creativity. Currently a
-				barista and studying full-time under the Pursuit Fellowship for a career
-				in tech, Bethany enjoys Dungeons & Dragons in her free time with their
-				friends, and has a passion for all things Halloween. Bethany uses
-				she/they pronouns and speaks English and Spanish fluently.
-			</p>
+      <h2>IRIS</h2>
+      <p></p>
+      <h2>MARCIA</h2>
+      <p>
+      <img src="src/assets/Marcia.jpeg" alt="Marcia" />
+      <br></br>
+        Marcia is a driven home-based bakery owner who's passionate about
+        creating custom cakes and treats. As a Pursuit fellow on the path to
+        full-stack software development, she merges her love for crafting from
+        scratch with her desire of learning to code. Marcia enjoys traveling,
+        exploring new cultures, and trying new foods.
+      </p>
+      <h2>BETHANY</h2>
+      <p>
+        Bethany (nicknamed Fig) is a freelance illustrator and multimedia artist
+        who has interest in all forms of expression and creativity. Currently a
+        barista and studying full-time under the Pursuit Fellowship for a career
+        in tech, Bethany enjoys Dungeons & Dragons in her free time with their
+        friends, and has a passion for all things Halloween. Bethany uses
+        she/they pronouns and speaks English and Spanish fluently.
+      </p>
 
-			<h2>ANICKA</h2>
-			<p>
-				Nicki is a multi-creative entrepreneur whom is updating her arsenal of
-				skills in order to positively impact communities and eventually the
-				world. She is a mobile cosmetologist specializing in Natural Hair Care.
-				In her free time (that doesn't really exist) she fine tunes her
-				interpersonal and intrapersonal skills through group collaborations
-				whilst navigating the complexities that is the individual human
-				existence.
-			</p>
+      <h2>ANICKA</h2>
+      <p>
+      <img src="src/assets/Nicki.jpg" alt="Nicki" />
+        Nicki is a multi-creative entrepreneur whom is updating her arsenal of
+        skills in order to positively impact communities and eventually the
+        world. She is a mobile cosmetologist specializing in Natural Hair Care.
+        In her free time (that doesn't really exist) she fine tunes her
+        interpersonal and intrapersonal skills through group collaborations
+        whilst navigating the complexities that is the individual human
+        existence.
+      </p>
 
-			<h2>DARYNA</h2>
-			<p></p>
-		</div>
-	);
+      <h2>DARYNA</h2>
+      <p>
+        Daryna is entrepreneurial marketing professional with background in
+        sales and event planning. Currently enrolled in Full Stack web
+        development school Pursuit. Experienced networker with vast contacts in
+        multiple industries in NYC and beyond. She is focusing on seeking out
+        top creative talent and bringing them together with other innovators to
+        create partnerships that generate amazing new products, campaigns and
+        long-lasting relationships. Daryna is also very passionate about the
+        startup industry and excited to build relationships with teams of people
+        building amazing products, and helping them make connections to people
+        who can help them achieve their vision. Fluent is English, Ukrainian and
+        English
+      </p>
+    </div>
+  );
 };
 
 export default AboutPage;

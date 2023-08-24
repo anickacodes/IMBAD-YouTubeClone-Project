170fdc865caa17092dcc94c5ca2c95876c96bd88
anickacodes <anickalewis@pursuit.org>
2023-08-24 02:01:55 -0400

    updated search function typo

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,14 +1,22 @@
 import { useState } from "react";
+// import VideosList from "./VideosList";
 
 
 const SearchBar = ({onSearch}) => {
 const [searchQuery, setSearchQuery] = useState('')
+ 
 
+    // function handleSearchInput(e) {
+    //  setSearchQuery(e.target.value);
+    // }
   
     function handleSubmit(searchInput){
       searchInput.preventDefault()
       onSearch(searchQuery)
     }
+  // what you type in the input has to match the fecth result's .title name 
+  //if on submiit/search result is valid, give it w first 8 videos 
+  //if its not valid, "no search results found"
   
     return (
       <>
@@ -26,7 +34,7 @@ const [searchQuery, setSearchQuery] = useState('')
           </button>
       
       </form>
-   
+      <hr />
       </>
     );
   };

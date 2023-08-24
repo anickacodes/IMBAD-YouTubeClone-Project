0d73b7116d9d447a064a7c80c2fe9cd499e73231
anickacodes <anickalewis@pursuit.org>
2023-08-24 12:34:53 -0400

    videos rendering on video page after error message, needing rework + replaced API key

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import './App.css';
 import AboutPage from './components/AboutPage';
 import { Route, Routes } from 'react-router-dom';
 import YouTubeAPIComponent from './components/YouTubeAPIComponent';
-import Home from './components/Home';
+// import Home from './components/Home';
 import NavBar from './components/NavBar';
 import SearchBar from './components/SearchBar';
 import { useState } from 'react';
@@ -12,43 +12,50 @@ function App() {
 	const [videos, setVideos] = useState([])
 	const [videosToRender, setVideoToRender] = useState([]);
 
-	function handleSearch(searchQuery) {
+	// function handleSearch(searchQuery) {
 		
-		const API_KEY = `${import.meta.env.VITE_REACT_APP_API_KEY}`;
+	// 	const API_KEY = `${import.meta.env.VITE_REACT_APP_API_KEY}`;
 		
-		const maxResults = 8;
-		const searchURL = `https://youtube.googleapis.com/youtube/v3/search?part=snippet&q=${encodeURIComponent(
-		  searchQuery
-		)}&type=video&maxResults=${maxResults}&key=${API_KEY}`;
+	// 	const maxResults = 8;
+	// 	const searchURL = `https://youtube.googleapis.com/youtube/v3/search?part=snippet&q=${encodeURIComponent(
+	// 	  searchQuery
+	// 	)}&type=video&maxResults=${maxResults}&key=${API_KEY}`;
 	
-		fetch(searchURL)
-		  .then((response) => response.json())
-		  .then((data) => {
-			setVideos(data.items)
-			setVideoToRender(data.items);
+	// 	fetch(searchURL)
+	// 	  .then((response) => response.json())
+	// 	  .then((data) => {
+	// 		setVideos(data.items)
+	// 		setVideoToRender(data.items);
 		
-		  })
-		  .catch((error) => {
-			console.error(error);
-		  });
-	  }
+	// 	  })
+	// 	  .catch((error) => {
+	// 		console.error(error);
+	// 	  });
+	//   }
 
 
 
 	return (
 		<>
-
+			
+			<NavBar />
+	
       <Routes>
-        <Route path="/" element={ <Home />} />
-        <Route path="/Home" element={ <Home />} />
+        <Route path="/" element={ <h1>Home</h1>} />
+    
         <Route path="/AboutPage" element={ <AboutPage />} />
-        <Route path="/Videos" element={ <div className="card videos"> <YouTubeAPIComponent videos={videos} setVideos={setVideos} setVideoToRender={setVideoToRender}/> </div>} />
+        <Route path="/Videos" element={ <div className="card videos"> <YouTubeAPIComponent/> </div>} />
       
       </Routes>
 			<div className="card videos">
 				<div>
 					{/* <h4>Videos Section PlaceHolder</h4> */}
-					<SearchBar onSearch={handleSearch}/>
+					<div className="card videos">
+				<div>
+					{/* <h4>Videos Section PlaceHolder</h4> */}
+
+				</div>
+			</div>
 
 				</div>
 			</div>
